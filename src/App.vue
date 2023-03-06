a8ed16ebe0681728264314a0f18a8a21d3366a67
Akbar Ismail <akbarismaiel@gmail.com>
2023-03-06 10:42:17 +0700

    update link

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <div class="flex">
       <div class="mr-5 p-3 bg-white">
         <img
-          src="https://nos.wjv-1.neo.id/himpsi-dev/profil-user/anggota/avatar/20230002.png"
+          src="https://nos.wjv-1.neo.id/himpsi-sik-prod/profil-user/anggota/1019/ERROR%20400.png"
           alt="acumalaka"
           class="w-[210px] h-[210px] object-cover"
         />
@@ -13,7 +13,7 @@
           bg="bg-himpsi-purple"
           @click="
             downloadContent(
-              'https://nos.wjv-1.neo.id/himpsi-dev/profil-user/anggota/avatar/20230002.png',
+              'https://nos.wjv-1.neo.id/himpsi-sik-prod/profil-user/anggota/1019/ERROR%20400.png',
             )
           "
         >
@@ -33,25 +33,6 @@
           @click="
             downloadContent(
               'https://nos.wjv-1.neo.id/himpsi-sik-prod/master/asosiasi/1/logo.png',
-            )
-          "
-        >
-          Unduh</BaseBtn
-        >
-      </div>
-      <div class="mr-5 p-3 bg-white">
-        <img
-          src="https://nos.wjv-1.neo.id/himpsi-dev/profil-user/anggota/avatar/20230111.png"
-          alt="acumalaka"
-          class="w-[210px] h-[210px] object-cover"
-        />
-        <BaseBtn
-          class="py-2 px-5 flex items-center mt-5"
-          flat
-          bg="bg-himpsi-purple"
-          @click="
-            downloadContent(
-              'https://nos.wjv-1.neo.id/himpsi-dev/profil-user/anggota/avatar/20230111.png',
             )
           "
         >
